9d003e132d2040e3556c8adc3a71be4bd1a75582
Mahak <chandnamahak@gmail.com>
2025-09-03 21:41:21 +0800

    users

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -8,6 +8,31 @@ export interface User{
     pwd: boolean,
     helper: boolean,
     viaPoints?: number,
-    createdDate: Date,
-    updatedDate: Date 
+    createdDate?: Date,
+    updatedDate?: Date 
+}
+
+export interface pwd{
+    userId: string,
+    userName: string,
+    email: string,
+    phoneNumber: number,
+    postalCode?: number,
+    homeAddress?: string,
+    pwd: boolean,
+    createdDate?: Date,
+    updatedDate?: Date 
+}
+
+export interface helper{
+    userId: string,
+    userName: string,
+    email: string,
+    phoneNumber: number,
+    postalCode?: number,
+    homeAddress?: string,
+    helper: boolean,
+    viaPoints?: number,
+    createdDate?: Date,
+    updatedDate?: Date 
 }
