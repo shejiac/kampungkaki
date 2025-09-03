763cd3dc6fee0b97bb1ab864126d0829e5cbc8a4
Mahak <chandnamahak@gmail.com>
2025-09-03 23:02:00 +0800

    request

--- backend/src/types/request.ts
+++ backend/src/types/request.ts
@@ -1,11 +1,13 @@
 export interface Request{
-    requesterId: string,
-    requestTitle: string,
-    requestType: string,
-    requestDescription: string,
-    requestLocation: string,
-    requestIntialMeet: boolean,
-    requestTime: number,
-    requestApproxDuration: number,
-    requestPriority: string, 
+    requester_id: string,
+    request_title: string,
+    request_type: string,
+    request_description: string,
+    request_location: string,
+    request_intial_meet: boolean,
+    request_time: number,
+    request_approx_duration: number,
+    request_priority: string, 
+    created_date?: Date,
+    updated_date?: Date
 }
