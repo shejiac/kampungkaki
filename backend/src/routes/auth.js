b20ea8b8fd8f7f03d371da664cbd728cf9d78e59
r19y <19ohsherry@gmail.com>
2025-09-06 00:56:19 +0800

    deleting of auth

--- backend/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-﻿import { Router } from "express";
-import { loginUser, getProfile } from "../controllers/authController.js";
-const router = Router();
-
-router.post("/login", loginUser);
-router.get("/profile", getProfile);
-
-export default router;
